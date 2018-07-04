bc58a18990ce1836506a5b340540cf05175e911d
Gerd Reiss <gerdreiss@jscriptive.com>
2018-07-04 21:00:27 +0200

    refactoring for monadic execution in echo

--- src/main/scala/chapter01/TerminalC.scala
+++ src/main/scala/chapter01/TerminalC.scala
@@ -14,13 +14,13 @@ object TerminalC {
   type Now[X] = X
 
   object TerminalSync extends Terminal[Now] {
-    override def read: Now[String] = ???
-    override def write(t: String): Now[Unit] = ???
+    override def read: Now[String] = "Sync read"
+    override def write(t: String): Now[Unit] = "Sync write"
   }
 
   object TerminalAsync extends Terminal[Future] {
-    override def read: Future[String] = ???
-    override def write(t: String): Future[Unit] = ???
+    override def read: Future[String] = Future { "Async read" }
+    override def write(t: String): Future[Unit] = Future { "Async write" }
   }
 
 
@@ -30,10 +30,40 @@ object TerminalC {
   }
 
 
-  def echo[C[_]](t: Terminal[C], e: Execution[C]): C[String] =
-    e.doAndThen(t.read) { in: String =>
-      e.doAndThen(t.write(in)) { _: Unit =>
-        e.create(in)
-      }
+  // this
+  // def echo[C[_]](t: Terminal[C], e: Execution[C]): C[String] =
+  //   e.doAndThen(t.read) { in: String =>
+  //     e.doAndThen(t.write(in)) { _: Unit =>
+  //       e.create(in)
+  //     }
+  //   }
+  //
+  // can be refactored to this, to enable a for comprehension over the execution
+  object Execution {
+
+    implicit class Ops[A, C[_]](c: C[A]) {
+      def flatMap[B](f: A => C[B])(implicit e: Execution[C]): C[B] =
+        e.doAndThen(c)(f)
+
+      def map[B](f: A => B)(implicit e: Execution[C]): C[B] =
+        e.doAndThen(c)(f andThen e.create)
     }
+
+  }
+
+  // here we use the implicit flatMap and map functions
+  //def echo[C[_]](implicit t: Terminal[C], e: Execution[C]): C[String] =
+  //  t.read.flatMap { in: String =>
+  //    t.write(in).map { _: Unit =>
+  //      in
+  //    }
+  //  }
+  //
+  // which can be rewritten into a for comprehension, or a monadic calculation
+  def echo[C[_]](implicit t: Terminal[C], e: Execution[C]): C[String] =
+    for {
+      in <- t.read
+      _  <- t.write(in)
+    } yield in
+
 }
